d8f7a400272c6955dc014cadce6a10bafb08c6b1
James Southern <jms301@gmail.com>
2014-11-10 11:42:21 +0000

    moved data files into .data directory to hide them from meteor. renamed loading script to 'dataload.sh' & removed spurious '2's from json file names

--- .data/dataload.sh
+++ .data/dataload.sh
@@ -1,5 +1,5 @@
 mongoimport -h 127.0.0.1:3002 -db meteor -c projects --file ./projects_load.json
-mongoimport -h 127.0.0.1:3002 -db meteor -c richcompanies --file ./companies_richard2.json
-mongoimport -h 127.0.0.1:3002 -db meteor -c transactions --file ./transactions2.json
+mongoimport -h 127.0.0.1:3002 -db meteor -c richcompanies --file ./companies_richard.json
+mongoimport -h 127.0.0.1:3002 -db meteor -c transactions --file ./transactions.json
 mongoimport -h 127.0.0.1:3002 -db meteor -c companies --file ./reconciled_companies.json
 mongoimport -h 127.0.0.1:3002 -db meteor -c naoreports --file ./nao_links.json
