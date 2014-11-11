321baf40e36c2275a9fcde3fe06e65869e4ed812
James Southern <jms301@gmail.com>
2014-11-11 14:28:36 +0000

    add remote data load script

---
+++ .data/remote-dataload.sh
@@ -0,0 +1,11 @@
+user=''
+pass=''
+host=''
+db=''
+
+#mongoimport --username $user --password $pass -h $host -db $db -c projects --file ./projects_load.json
+#mongoimport --username $user --password $pass -h $host -db $db -c richcompanies --file ./companies_richard.json
+#mongoimport --username $user --password $pass -h $host -db $db -c transactions --file ./transactions.json
+#mongoimport --username $user --password $pass -h $host -db $db -c companies --file ./reconciled_companies.json
+#mongoimport --username $user --password $pass -h $host -db $db -c naoreports --file ./nao_links.json
+#mongoimport --username $user --password $pass -h $host -db $db -c nationalcharts --file ./national_charts.json
