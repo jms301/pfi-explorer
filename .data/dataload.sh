6cb0714d7bcfbedc988f16cdb58a10c62c4c512d
Richard Palmer <richard@tiro.org.uk>
2015-01-31 17:37:44 +0000

    Load new data up

--- .data/dataload.sh
+++ .data/dataload.sh
@@ -4,3 +4,4 @@ mongoimport -h 127.0.0.1:3002 -db meteor -c transactions --file ./transactions.j
 mongoimport -h 127.0.0.1:3002 -db meteor -c companies --file ./reconciled_companies.json
 mongoimport -h 127.0.0.1:3002 -db meteor -c naoreports --file ./nao_links.json
 mongoimport -h 127.0.0.1:3002 -db meteor -c nationalcharts --file ./national_charts.json 
+mongoimport -h 127.0.0.1:3002 -db meteor -c departments --file ./departments_by_year.json 
